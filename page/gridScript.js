fea78868c85b2623c355690afce38b1a8de8b058
haved <krogstie.havard@gmail.com>
2018-03-27 00:40:49 +0200

    Make nice loading wheel

--- page/gridScript.js
+++ page/gridScript.js
@@ -12,15 +12,29 @@ function fixSize(ctx) {
 	canvas.height = height;
 }
 
-function drawLoading(ctx) {
-	ctx.
+function drawLoading(ctx, theta) {
+	ctx.fillStyle="#EEEEEE";
 	ctx.fillRect(0, 0, width, height);
+
+	ctx.lineWidth = 5;
+	const sin = Math.sin(theta)*60;
+	const cos = Math.cos(theta)*60;
+	let grad = ctx.createLinearGradient(width/2-sin, height/2+cos, width/2+sin, height/2-cos);
+	grad.addColorStop("0","black");
+	grad.addColorStop("1","white");
+	ctx.strokeStyle=grad;
+
+	ctx.beginPath();
+	ctx.arc(width/2, height/2, 50, theta-Math.PI/2, theta+Math.PI);
+	ctx.stroke();
+
+	requestAnimationFrame(()=>drawLoading(ctx, theta+0.04));
 }
 
 
 function drawGrid(ctx) {
 	fixSize(ctx);
-	drawLoading(ctx);
+	drawLoading(ctx, 0);
 }
 
 function start() {
